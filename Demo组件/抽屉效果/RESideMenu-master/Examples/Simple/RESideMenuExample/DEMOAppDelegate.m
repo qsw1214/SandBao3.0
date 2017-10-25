98f2671ffe41e385433067044e93c767461f6950
tianNanYiHao <851085835@qq.com>
2017-10-25 17:09:46 +0800

    123

---
+++ Demo组件/抽屉效果/RESideMenu-master/Examples/Simple/RESideMenuExample/DEMOAppDelegate.m
@@ -0,0 +1,71 @@
+//
+//  DEMOAppDelegate.m
+//  RESideMenuExample
+//
+//  Created by Roman Efimov on 10/10/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import "DEMOAppDelegate.h"
+#import "DEMOLeftMenuViewController.h"
+#import "DEMORightMenuViewController.h"
+#import "DEMOFirstViewController.h"
+
+@implementation DEMOAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:[[DEMOFirstViewController alloc] init]];
+    DEMOLeftMenuViewController *leftMenuViewController = [[DEMOLeftMenuViewController alloc] init];
+    DEMORightMenuViewController *rightMenuViewController = [[DEMORightMenuViewController alloc] init];
+    
+    RESideMenu *sideMenuViewController = [[RESideMenu alloc] initWithContentViewController:navigationController
+                                                                    leftMenuViewController:leftMenuViewController
+                                                                   rightMenuViewController:nil];
+    sideMenuViewController.backgroundImage = [UIImage imageNamed:@"Stars"];
+    
+    sideMenuViewController.menuPreferredStatusBarStyle = 1; // UIStatusBarStyleLightContent
+    //设置代理
+    sideMenuViewController.delegate = self;
+    sideMenuViewController.contentViewShadowColor = [UIColor blackColor];
+    //设置阴影的偏移量
+    sideMenuViewController.contentViewShadowOffset = CGSizeMake(-40, 0);
+    //设置阴影的不透明
+    sideMenuViewController.contentViewShadowOpacity = 0.6;
+    //设置阴影的圆角
+    sideMenuViewController.contentViewShadowRadius = 12;
+    //设置阴影的显示
+    sideMenuViewController.contentViewShadowEnabled = NO;
+    self.window.rootViewController = sideMenuViewController;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+#pragma mark -
+#pragma mark RESideMenu Delegate
+
+- (void)sideMenu:(RESideMenu *)sideMenu willShowMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"willShowMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+- (void)sideMenu:(RESideMenu *)sideMenu didShowMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"didShowMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+- (void)sideMenu:(RESideMenu *)sideMenu willHideMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"willHideMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+- (void)sideMenu:(RESideMenu *)sideMenu didHideMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"didHideMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+@end
