961601be090f5a75556fb77ed1e66ecec8242abc
tianNanYiHao <851085835@qq.com>
2017-12-06 14:40:27 +0800

    12
    sps

--- sps/spsTest/spsTest/ViewController.m
+++ sps/spsTest/spsTest/ViewController.m
@@ -22,21 +22,33 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    self.navigationController.automaticallyAdjustsScrollViewInsets = NO;
+}
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     
     RXSPSEntry*sps = [RXSPSEntry sharedInstance];
-    
+    sps.view.backgroundColor = [UIColor greenColor];
     sps.delegate = self ;
-
+    
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:sps];
     [self.navigationController presentViewController:navigationController animated:YES completion:nil];
-
     
     NSString *path = [[NSBundle mainBundle] pathForResource:@"123" ofType:@"json"];
     NSData *data = [NSData dataWithContentsOfFile:path];
     NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     
-    
     [sps CallSps:str];
+    
+}
+
+
+- (void)RXSPSReturn:(NSString *)jsonData{
+    
+    NSLog(@" \n-=-=-=- \n %@ \n-=-=-=-",jsonData);
     
 }
 
